c84141d54ce79ce5d09b7234f42aabf6c3cde4aa
Azizul Islam <azizulislam5360@gmail.com>
2026-02-01 20:30:12 +0600

    Add files via upload

---
+++ 1.Python/Chapter7/Practise2.py
@@ -0,0 +1,9 @@
+'''Write a program to greet all the person names stored in a list l and which starts with S.'''
+
+l=["Harry","Sohan","Sachin","Rahul","Samar","Vikram"]
+
+for i in l:
+    if (i.startswith("S")):
+        print("Hello " + i)
+    else:
+        pass
