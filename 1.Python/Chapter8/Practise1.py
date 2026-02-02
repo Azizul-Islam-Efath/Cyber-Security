91c860578212e8045a110f5ecf542d6f182abde4
Azizul Islam <azizulislam5360@gmail.com>
2026-02-02 21:44:03 +0600

    Add files via upload

---
+++ 1.Python/Chapter8/Practise1.py
@@ -0,0 +1,12 @@
+
+
+
+def greet():
+     name=str(input("Enter your name: "))
+     age=int(input("Enter your age: "))
+     print("Hello", name, "Take Greetings! You are", age, "years old.")
+
+
+
+greet()
+greet()
