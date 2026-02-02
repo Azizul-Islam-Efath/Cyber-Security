1bb864b162dfdab3ab89165e96f5455bb27da1c5
Azizul Islam <azizulislam5360@gmail.com>
2026-02-02 21:43:06 +0600

    Add files via upload

---
+++ 1.Python/Chapter8/Func with arg.py
@@ -0,0 +1,14 @@
+
+
+
+def greet(name):
+     print(f"\n\nHello {name}. Help us by giving your info.\n")
+     Gender=str(input("\nEnter your Gender:(Male/Female)?"))
+     age=int(input("\nEnter your age: "))
+     Address=str(input("\nEnter your address: "))
+     print(f"\n\nThanks, {name}Take Greetings!\n\nYou are a {Gender}.\n\nYou are {age} years old.\n\nAnd Your Address is: {Address}")
+
+
+
+greet("Azizul Islam.")
+greet("Sadia Jaman Mishka.")
