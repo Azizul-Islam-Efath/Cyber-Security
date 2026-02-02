3c3f661efabdc4bec579aa07ff800af29e8d0ac3
Azizul Islam <azizulislam5360@gmail.com>
2026-02-02 21:42:42 +0600

    Add files via upload

--- 1.Python/Chapter8/Function.py
+++ 1.Python/Chapter8/Function.py
@@ -1,1 +1,13 @@
 
+#function definition
+def avg():
+    a=int(input("Enter first number: "))
+    b=int(input("Enter second number: "))
+    c=int(input("Enter third number: "))
+    average=(a+b+c)/3
+    print("The average of three numbers is:", average)      
+
+
+avg()  # calling function
+avg()   #calling function
+avg()
